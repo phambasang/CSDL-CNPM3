47cb6b2f6db0b95722bec5f49dc6dd8bac6c1b25
phambasang <118547220+phambasang@users.noreply.github.com>
2023-03-08 11:25:28 +0700

    Add files via upload

---
+++ BÀI THỰC HÀNH SQL SERVER – BUỔI 06.sql
@@ -0,0 +1,105 @@
+﻿---1
+SELECT (NHANVIEN.HONV + ' ' + NHANVIEN.TENLOT + ' '+ NHANVIEN.TENNV) AS 'Họ tên nhân viên có ít nhất 1 thân nhân'
+	FROM NHANVIEN, THANNHAN
+	WHERE NHANVIEN.MANV= THANNHAN.MA_NVIEN
+	GROUP BY (NHANVIEN.HONV + ' ' + NHANVIEN.TENLOT + ' '+ NHANVIEN.TENNV)
+	HAVING COUNT(THANNHAN.MA_NVIEN) > 1
+---2
+SELECT (NHANVIEN.HONV + ' ' + NHANVIEN.TENLOT + ' '+ NHANVIEN.TENNV) AS 'Họ tên nhân viên không có thân nhân'
+	FROM NHANVIEN
+	WHERE NHANVIEN.MANV NOT IN (SELECT THANNHAN.MA_NVIEN
+								FROM NHANVIEN, THANNHAN
+								WHERE NHANVIEN.MANV = THANNHAN.MA_NVIEN
+								)
+---3. Cho biết họ tên các nhân viên có trên 2 thân nhân.
+SELECT (NHANVIEN.HONV + ' ' + NHANVIEN.TENLOT + ' '+ NHANVIEN.TENNV) AS 'Họ tên nhân viên có trên 2 thân nhân'
+	FROM NHANVIEN, THANNHAN
+	WHERE NHANVIEN.MANV= THANNHAN.MA_NVIEN
+	GROUP BY (NHANVIEN.HONV + ' ' + NHANVIEN.TENLOT + ' '+ NHANVIEN.TENNV)
+	HAVING COUNT(THANNHAN.MA_NVIEN) > 2
+---4. Cho biết họ tên những trưởng phòng có ít nhất một thân nhân.
+SELECT (NHANVIEN.HONV + ' ' + NHANVIEN.TENLOT + ' '+ NHANVIEN.TENNV) AS 'Họ tên trưởng phòng có ít nhất 1 thân nhân'
+	FROM NHANVIEN, PHONGBAN
+	WHERE NHANVIEN.MANV = PHONGBAN.TRPHG AND
+		  PHONGBAN.TRPHG IN (SELECT THANNHAN.MA_NVIEN
+							 FROM NHANVIEN, THANNHAN
+							 WHERE NHANVIEN.MANV = THANNHAN.MA_NVIEN
+							 )
+--Cau6
+SELECT (NHANVIEN.HONV + ' ' + NHANVIEN.TENLOT + ' ' + NHANVIEN.TENNV) AS 'Họ tên nhân viên có lương trung bình trên mức lương trung bình của phòng "Quản lý"'
+	FROM NHANVIEN
+	WHERE NHANVIEN.LUONG > (SELECT AVG(NHANVIEN.LUONG)
+							FROM NHANVIEN, PHONGBAN
+							WHERE NHANVIEN.PHG = PHONGBAN.MAPHG AND
+								  PHONGBAN.TENPHG = N'Quản lý')
+--Cau7
+SELECT (NHANVIEN.HONV + ' ' + NHANVIEN.TENLOT + ' ' + NHANVIEN.TENNV) AS 'Họ tên nhân viên có lương trung bình trên mức lương trung bình của phòng đang làm việc'
+	FROM NHANVIEN
+	WHERE NHANVIEN.LUONG > (SELECT AVG(NHANVIEN.LUONG)
+							FROM NHANVIEN, PHONGBAN
+							WHERE NHANVIEN.PHG = PHONGBAN.MAPHG )
+--8
+SELECT PHONGBAN.TENPHG, (NHANVIEN.HONV + ' ' + NHANVIEN.TENLOT + ' ' + NHANVIEN.TENNV) AS 'Họ tên trưởng phòng của phòng ban đông nhân viên nhất'
+	FROM NHANVIEN, PHONGBAN
+	WHERE NHANVIEN.MANV = PHONGBAN.TRPHG AND
+		  PHONGBAN.MAPHG = (SELECT TOP 1 PHONGBAN.MAPHG
+							FROM NHANVIEN, PHONGBAN
+							WHERE NHANVIEN.PHG = PHONGBAN.MAPHG
+							GROUP BY PHONGBAN.MAPHG
+							ORDER BY COUNT (NHANVIEN.PHG) DESC
+							)
+---9. Cho biết danh sách các đề án mà nhân viên có mã là 456 chưa tham gia.
+SELECT DEAN.MADA
+	FROM DEAN
+	WHERE DEAN.MADA NOT IN (SELECT PHANCONG.MADA
+							FROM PHANCONG
+							WHERE PHANCONG.MA_NVIEN = '456'
+							)
+---11
+	SELECT DISTINCT (NHANVIEN.HONV + ' ' + NHANVIEN.TENLOT + ' ' + NHANVIEN.TENNV) AS 'Họ tên nhân viên', NHANVIEN.DCHI
+	FROM NHANVIEN, DEAN, DIADIEM_PHG
+	WHERE NHANVIEN.PHG = DEAN.PHONG AND
+		  NHANVIEN.PHG = DIADIEM_PHG.MAPHG AND
+		  DEAN.DDIEM_DA IN (SELECT DEAN.DDIEM_DA
+							FROM DEAN
+							) AND
+		  DIADIEM_PHG.DIADIEM NOT LIKE DEAN.DDIEM_DA
+
+
+
+12. Để lấy danh sách các mã đề án có: nhân công với họ là Lê hoặc có người trưởng phòng chủ trì đề án với họ là Lê, ta có thể sử dụng câu lệnh sau:
+
+SELECT DISTINCT d.MADA
+FROM DeAn d, NhanVien nv, PhanCong pc
+WHERE d.MADA = pc.MADA
+AND (nv.HONV LIKE 'Lê%' OR d.DDIEM_DA IN (SELECT MANV FROM NhanVien WHERE HONV LIKE 'Lê%')) 
+AND pc.MA_NVIEN = nv.MANV;
+
+
+13. Để liệt kê danh sách các đề án mà cả hai nhân viên có mã số 123 và 789 cùng làm, ta có thể sử dụng câu lệnh sau:
+
+SELECT DISTINCT d.MADA
+FROM DeAn d, PhanCong pc1, PhanCong pc2 
+WHERE d.MADA = pc1.MADA AND pc1.MADA = pc2.MADA
+AND pc1.MA_NVIEN= 123 AND pc2.MA_NVIEN = 789;
+
+--14
+SELECT DA.MADA, DA.TENDA
+FROM DeAn DA
+JOIN PhanCong PC1 ON DA.MADA = PC1.MADA
+JOIN PhanCong PC2 ON DA.MADA = PC2.MADA
+JOIN NhanVien NV1 ON PC1.MA_NVIEN = NV1.MANV AND NV1.HONV = 'Đinh Bá Tiến'
+JOIN NhanVien NV2 ON PC2.MA_NVIEN = NV2.MANV AND NV2.HONV = 'Trần Thanh Tâm'
+
+--15
+SELECT NV.MaNV, NV.HONV, NV.Phai
+FROM NHANVIEN NV
+WHERE NOT EXISTS (
+    SELECT DA.MaDA
+    FROM DEAN DA
+    WHERE NOT EXISTS (
+        SELECT * 
+        FROM PHANCONG PC 
+        WHERE PC.MA_NVIEN = NV.MaNV AND PC.MaDA = DA.MaDA
+    )
+)
---
+++ KIỂM TRA THỰC HÀNH – BÀI SỐ 1.sql
@@ -0,0 +1,164 @@
+﻿--Câu1:
+-- Tạo login cho trưởng nhóm trưởng nhóm
+CREATE LOGIN TruongNhom WITH PASSWORD = '12042002';
+GO
+
+-- Tạo user cho trưởng nhóm trưởng nhóm
+USE AdventureWorks2008R2;
+CREATE USER TruongNhom FOR LOGIN TruongNhom;
+GO
+
+-- Tạo login cho nhân viên NV
+CREATE LOGIN NhanVien WITH PASSWORD = '12042002';
+GO
+
+-- Tạo user cho nhân viên NV
+USE AdventureWorks2008R2;
+CREATE USER NhanVien FOR LOGIN NhanVien;
+GO
+
+-- Tạo login cho nhân viên QuanLy
+CREATE LOGIN QuanLy WITH PASSWORD = '12042002';
+GO
+
+-- Tạo user cho nhân viên QL
+USE AdventureWorks2008R2;
+CREATE USER QuanLy FOR LOGIN QuanLy;
+GO
+
+--b. Phân quyền cho các nhân viên:
+
+-- Phân quyền cho trưởng nhóm TN
+USE AdventureWorks2008R2;
+GRANT SELECT, UPDATE,DELETE ON Production.ProductInventory TO TruongNhom;
+GO
+
+-- Phân quyền cho nhân viên NV
+USE AdventureWorks2008R2;
+GRANT SELECT,UPDATE, DELETE ON Production.ProductInventory TO NhanVien;
+GO
+
+-- Phân quyền cho nhân viên QL
+USE AdventureWorks2008R2;
+GRANT SELECT ON Production.ProductInventory TO QuanLy;
+GO
+
+-- Admin phải có quyền CONTROL trên tất cả các đối tượng trong cơ sở dữ liệu
+USE AdventureWorks2008R2;
+GRANT CONTROL TO [Admin];
+GO
+
+--c. Đăng nhập và thực hiện các yêu cầu:
+
+-- Đăng nhập với tài khoản của trưởng nhóm TN
+USE AdventureWorks2008R2;
+EXECUTE AS USER = 'TN';
+
+-- Sửa 1 dòng dữ liệu tùy ý trong bảng Production.ProductInventory
+UPDATE Production.ProductInventory
+SET Quantity = 20
+WHERE ProductID = 1;
+
+-- Kết thúc quyền của trưởng nhóm TN
+REVERT;
+
+-- Đăng nhập với tài khoản của nhân viên NV
+USE AdventureWorks2008R2;
+EXECUTE AS USER = 'NV';
+
+-- Xóa 1 dòng dữ liệu tùy ý trong bảng Production.ProductInventory
+DELETE FROM Production.ProductInventory
+WHERE ProductID = 2;
+
+-- Kết thúc quyền của nhân viên NV
+REVERT;
+
+-- Đăng nhập với tài khoản của nhân viên QL
+USE AdventureWorks2008R2;
+EXECUTE AS USER = 'QL';
+
+-- Xem lại kết quả thực hiện của trưởng nhóm TN và nhân viên NV
+SELECT * FROM Production.ProductInventory;
+
+-- Kết thúc quyền của nhân viên QL
+REVERT;
+
+d. Ai có thể sửa được dữ liệu bảng Production.Product ?
+
+Chỉ có trưởng nhóm TN và nhân viên QL có thể sửa được dữ liệu bảng Production.Product, vì họ được phân quyền SELECT và UPDATE trên bảng này.
+
+e. Thu hồi quyền cấp cho nhân viên NV:
+
+-- Thu hồi quyền của nhân viên NV
+USE AdventureWorks2008R2;
+DELETE FROM sys.database_principals WHERE name = 'NV';
+
+USE AdventureWorks2008R2;
+DELETE FROM Production.ProductInventory WHERE EmployeeID = 'NV';
+
+USE AdventureWorks2008R2;
+REVOKE SELECT, DELETE ON Production.ProductInventory FROM NV;
+GO
+
+-- Xóa user của nhân viên NV
+USE AdventureWorks2008R2;
+DROP USER NV;
+GO
+
+
+--Câu 2:Thực hiện chuỗi các thao tác sau để có thể phục hồi database khi có sự cố ở thời điểm T8?
+
+-- 1: Thực hiện Full Backup
+BACKUP DATABASE AdventureWorks2008R2
+TO DISK = 'd:\path\AdventureWorks2008R2_Full.bak'
+--2 Cập nhật tăng mức tồn kho an toàn SafetyStockLevel trong table Production.Product lên 10% cho các mặt hàng là nguyên liệu sản xuất.
+UPDATE Production.Product
+SET SafetyStockLevel = SafetyStockLevel * 1.1
+WHERE FinishedGoodsFlag = 0;
+--3: Thực hiện Differential Backup
+BACKUP DATABASE AdventureWorks2008R2
+TO DISK = 'd:\path\AdventureWorks2008R2_Diff1.bak'
+WITH DIFFERENTIAL
+--4 Xóa mọi bản ghi trong bảng Person.Emailaddress
+DELETE FROM Person.EmailAddress
+--5Thực hiện Differential Backup
+BACKUP DATABASE AdventureWorks2008R2
+TO DISK = 'd:\path\AdventureWorks2008R2_Diff2.bak'
+WITH DIFFERENTIAL
+--6 Thêm một dòng trong table Person.ContactType
+INSERT INTO Person.ContactType (Name)
+VALUES ('Assistant')
+--7: Thực hiện Log Backup
+BACKUP LOG AdventureWorks2008R2
+TO DISK = 'd:\path\AdventureWorks2008R2_Log1.bak'
+--8:BACKUP LOG AdventureWorks2008R2
+DROP DATABASE AdventureWorks2008R2
+--9
+-- Phục hồi Full backup
+RESTORE DATABASE AdventureWorks2008R2
+FROM DISK = 'd:\path\AdventureWorks2008R2_Full.bak'
+WITH NORECOVERY;
+
+-- Phục hồi Differential backup gần nhất
+RESTORE DATABASE AdventureWorks2008R2
+FROM DISK = 'd:\path\AdventureWorks2008R2_Diff2.bak'
+WITH NORECOVERY;
+
+-- Phục hồi Differential backup xa hơn
+RESTORE DATABASE AdventureWorks2008R2
+FROM DISK = 'd:\path\AdventureWorks2008R2_Diff1.bak'
+WITH NORECOVERY;
+
+-- Phục hồi các transaction log
+RESTORE LOG [database_name]
+FROM DISK = 'd:\path\AdventureWorks2008R2_Diff3.bak'
+WITH NORECOVERY;
+RESTORE LOG [database_name]
+FROM DISK = 'd:\path\AdventureWorks2008R2_Diff4.bak'
+WITH NORECOVERY;
+
+
+--10
+RESTORE HEADERONLY FROM DISK='d:\path_to_backup_file.bak'
+
+
